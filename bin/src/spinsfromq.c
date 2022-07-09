103bad839dbb327c7ed4e5ec548505ae0ac726a1
Martin Rotter <martin@Martins-MacBook-Air.local>
2022-07-09 18:02:48 +0200

    spinsfromq.c phases for double and triple q correctly introduced

--- bin/src/spinsfromq.c
+++ bin/src/spinsfromq.c
@@ -26,9 +26,12 @@ printf("#*****************************************************\n");
 		h k l ....... components of qvector\n \
                 options:\n \
                 -m f1 f2 f3 ...fnofcomponents: multiply spin components by f1 f2 f3 ... fnofcomponents\n \
-      formulas: M(r)=(f1*M1,f2*M2,f3*M3)*cos(Q.r)    ...for single q\n \
+      formulas: M(r)=(f1*M1,f2*M2,f3*M3,..., fnofcomponents * Mnofcomponents)*cos(Q.r)    ...for single q\n \
                 M(r)=f1*M1*(1 0 0)*cos(Q1.r)+f2*M2*(0 1 0)*cos(Q2.r) ...for double q\n \
-                M(r)=f1*M1*(1 0 0)*cos(Q1.r)+f2*M2* (0 1 0)*cos(Q2.r)+f3*M3*(0 0 1)*cos(Q3.r) ...for triple q\n");
+                M(r)=f1*M1*(1 0 0)*cos(Q1.r)+f2*M2* (0 1 0)*cos(Q2.r)+f3*M3*(0 0 1)*cos(Q3.r) ...for triple q\n \
+      M1,M2,M3 are determined by applying an molecular field with equal components and increasing this \n \
+      field until the total length of the moment exceeds a treshold value. \n \
+");
       exit (1);
     }
 
@@ -70,8 +73,6 @@ printf("#*****************************************************\n");
   for(i=1;i<=inputpars.nofatoms;++i)
   {(*inputpars.jjj[i]).Icalc(moment,T,h,hext,lnz,u,(*inputpars.jjj[i]).Icalc_parameter_storage_init(h,hext,T));
    for(j=1;j<=inputpars.nofcomponents;++j){nettom(j+(i-1)*inputpars.nofcomponents)=moment(j);
-                                          // set phases according to atomic position phi=2*pi*q*r
-                                           phi(j+(i-1)*inputpars.nofcomponents)=qvector*(*inputpars.jjj[i]).xyz*2.0*PI;                                        
                                           }
     nettom1(1+(i-1)*inputpars.nofcomponents)=moment(1);
     nettom2(2+(i-1)*inputpars.nofcomponents)=moment(2);
@@ -79,20 +80,25 @@ printf("#*****************************************************\n");
   }
   h*=2; // multiply field h by two until moment netoom gets large enough to have some sizable numbers
   }
+  printf("#! Moment components: ");
   for(i=1;i<=inputpars.nofatoms;++i)
-  {for(j=1;j<=inputpars.nofcomponents;++j){nettom(j+(i-1)*inputpars.nofcomponents)*=factors(j);
+  {for(j=1;j<=inputpars.nofcomponents;++j){if(i==1)printf("M%i=%6.4f ",j,nettom(j+(i-1)*inputpars.nofcomponents));
+                                           nettom(j+(i-1)*inputpars.nofcomponents)*=factors(j);
                                           }
     nettom1(1+(i-1)*inputpars.nofcomponents)*=factors(1);
     nettom2(2+(i-1)*inputpars.nofcomponents)*=factors(2);
     nettom3(3+(i-1)*inputpars.nofcomponents)*=factors(3);                                          
   }
 
-  printf("#! n1=%i n2=%i n3=%i nofspins=%i\n",n1,n2,n3,n1*n2*n3*inputpars.nofatoms);
+  printf("\n#! n1=%i n2=%i n3=%i nofspins=%i\n",n1,n2,n3,n1*n2*n3*inputpars.nofatoms);
   printf("#! q: hkl=%g %g %g:\n",qvector(1),qvector(2),qvector(3));
+  for(i=1;i<=inputpars.nofatoms;++i)for(j=1;j<=inputpars.nofcomponents;++j){// set phases according to atomic position phi=2*pi*q*r
+                                           phi(j+(i-1)*inputpars.nofcomponents)=qvector*(*inputpars.jjj[i]).xyz*2.0*PI;                                        
+                                          }
+  
   // transform hkl to primitive reciprocal lattice
   qvector=qvector*inputpars.rez.Inverse();
   printf("# Miller indices of q vector with respect to primitive reciprocal lattice: (%6.4f %6.4f %6.4f)\n",qvector(1),qvector(2),qvector(3));
-  
   
  
   spincf savspin (n1,n2,n3,inputpars.nofcomponents,inputpars.nofatoms);
@@ -112,6 +118,9 @@ printf("#*****************************************************\n");
    qvector(2)=strtod(argv[a+8],NULL);
    qvector(3)=strtod(argv[a+9],NULL);
    printf("#! q2: hkl2=%g %g %g:\n",qvector(1),qvector(2),qvector(3));
+   for(i=1;i<=inputpars.nofatoms;++i)for(j=1;j<=inputpars.nofcomponents;++j){// set phases according to atomic position phi=2*pi*q*r
+                                           phi(j+(i-1)*inputpars.nofcomponents)=qvector*(*inputpars.jjj[i]).xyz*2.0*PI;                                        
+                                          }
    qvector=qvector*inputpars.rez.Inverse();
    printf("# Miller indices of q2 with respect to primitive reciprocal lattice: (%6.4f %6.4f %6.4f)\n",qvector(1),qvector(2),qvector(3));
    savspin2.spinfromq (n1,n2,n3,qvector,nettom2, momentq0, phi);
@@ -123,6 +132,9 @@ printf("#*****************************************************\n");
     qvector(2)=strtod(argv[a+11],NULL);
     qvector(3)=strtod(argv[a+12],NULL);
    printf("#! q3: hkl3=%g %g %g:\n",qvector(1),qvector(2),qvector(3));
+   for(i=1;i<=inputpars.nofatoms;++i)for(j=1;j<=inputpars.nofcomponents;++j){// set phases according to atomic position phi=2*pi*q*r
+                                           phi(j+(i-1)*inputpars.nofcomponents)=qvector*(*inputpars.jjj[i]).xyz*2.0*PI;                                        
+                                          }
    qvector=qvector*inputpars.rez.Inverse();
    printf("# Miller indices of q3 with respect to primitive reciprocal lattice: (%6.4f %6.4f %6.4f)\n",qvector(1),qvector(2),qvector(3));
    savspin2.spinfromq (n1,n2,n3,qvector,nettom3, momentq0, phi);
